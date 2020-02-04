ac3a595411d457dcbe5a0bd43798e907281b29e3
Lee <notayogilee@gmail.com>
2020-02-04 00:34:44 +0000

    https://github.com/notayogilee/lotide

---
+++ tail.js
@@ -0,0 +1,19 @@
+const tail = function(input) {
+  let newTail = [];
+  for (let i = 1; i < input.length; i++) {
+    newTail.push(input[i]);
+  }
+  return newTail;
+};
+
+
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`😄 Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
