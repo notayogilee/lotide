c63fae66c73384d71a469af2c4546e58d257be5d
Lee <notayogilee@gmail.com>
2020-02-05 01:30:08 +0000

    assertion completed

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  let actual = true;
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] !== arr1[i]) {
+      actual = false;
+    }
+  }
+  return actual;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`😄 Assertion Passed:  ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🙁 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
