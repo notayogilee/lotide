93788971f86e270b3d1a9134ab7f77fcd3edb6c7
Lee <notayogilee@gmail.com>
2020-02-06 14:05:38 +0000

    improved...

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,10 @@
 const assertEqual = function(actual, expected) {
-
   if (actual === expected) {
-    console.log('😄 Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`😄 Assertion Passed:  ${actual} === ${expected}`);
   } else {
-    console.log('🙁 Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🙁 Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
-assertEqual('Bootcamp', 'Bootcamp');
-assertEqual(1, 7);
+assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
