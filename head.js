a1081694d2836963a6432dd405bc927f9ac28aa0
Lee <notayogilee@gmail.com>
2020-02-10 22:31:14 +0000

    middle updated using mocha chai assertions

--- head.js
+++ head.js
@@ -5,5 +5,5 @@ const head = function (actual) {
 };
 
 module.exports = head;
-// console.log(assertEqual(head([]), 7));
-// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
+// middle([1, 2, 31, 4, 5]);
+// middle([21, 3, 31, 6]);
+// console.log(assertArraysEqual(middle([1, 2, 31, 4, 5]), [31], true)); // => should PASS
+// console.log(assertArraysEqual(middle([21, 3, 31, 6]), [3, 31], true));
+// // eqArrays([1, 2, 3], [1, 2, 3]);
 
-middle([1, 2, 31, 4, 5]);
-middle([21, 3, 31, 6]);
-console.log(assertArraysEqual(middle([1, 2, 31, 4, 5]), [31], true)); // => should PASS
-console.log(assertArraysEqual(middle([21, 3, 31, 6]), [3, 31], true));
-// eqArrays([1, 2, 3], [1, 2, 3]);
+describe('#middle', () => {
+  it('returns [3, 31] for [21, 3, 31, 6]', () => {
+    assert.deepEqual(middle([21, 3, 31, 6]), [3, 31]);
+  }),
+    it('returns [5] for [1, 5, 9]', () => {
+      assert.deepEqual(middle([1, 5, 9]), [5]);
+    });
+})
