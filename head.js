2369d8766c678c1ecdae8de221c2212e60f0fa26
Lee <notayogilee@gmail.com>
2020-02-10 17:37:37 +0000

    updated assertEquals using require(./assertEqual)

--- head.js
+++ head.js
@@ -4,16 +4,6 @@ const head = function (actual) {
   return actual[0];
 };
 
-// const assertEqual = function (actual, expected) {
-
-//   if (actual === expected) {
-//     return `😄 Assertion Passed:  ${actual} === ${expected}`;
-//   } else {
-//     return `🙁 Assertion Failed: ${actual} !== ${expected}`;
-//   }
-// };
-
-
 
 console.log(assertEqual(head([]), 7));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
