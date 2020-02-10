3abe61d89503ec9a09de50bcf4cd9fc5dbce49a9
Lee <notayogilee@gmail.com>
2020-02-10 17:30:16 +0000

    updated lotide

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     return `😄 Assertion Passed:  ${actual} === ${expected}`;
+//   } else {
+//     return `🙁 Assertion Failed: ${actual} !== ${expected}`;
+//   }
+// };
+module.exports = assertEqual;
